24f3c7b1a9179c4e7051c7b369b6dc84bb69c82d
Iulian Crisan <iuliancrisan01@gmail.com>
2021-02-02 18:48:50 +0200

    Installed packages, removed unused files

---
+++ src/theme/theme.js
@@ -0,0 +1,80 @@
+const THEME = {
+  spacing: {
+    unit: '8px',
+  },
+
+  fontWeight: {
+    light: 300,
+    regular: 400,
+    medium: 500,
+    bold: 700,
+    black: 900,
+  },
+
+  fontSize: {
+    bigTitle: '5rem',
+    title: '3rem',
+    subtitle: '2rem',
+    button: '1.6rem',
+    body: '1.4rem',
+    label: '1.2rem',
+  },
+
+  borderRadius: {
+    small: '0.4rem',
+    medium: '0.8rem',
+    big: '1.2rem',
+  },
+
+  shadow: {
+    accent: '0px 10px 20px rgba(0, 122, 255, 0.4)',
+    light: '0 10px 20px rgba(0, 0, 0, 0.05)',
+    medium: '0 10px 20px rgba(0, 0, 0, 0.14)',
+    primary:
+      '0 3px 6px -4px rgba(0, 0, 0, 0.12), 0 6px 16px 0 rgba(0, 0, 0, 0.08), 0 9px 28px 8px rgba(0, 0, 0, 0.05)',
+  },
+
+  backdropFilter: 'saturate(180%) blur(20px)',
+
+  color: {
+    accent: 'rgb(0, 122, 255)',
+    backgroundAccent: '#F0F7FF',
+
+    blue: 'rgb(0, 122, 255)',
+    green: 'rgb(52, 199, 89)',
+    indigo: 'rgb(88, 86, 214)',
+    orange: 'rgb(255, 149, 0)',
+    pink: 'rgb(255, 45, 85)',
+    purple: 'rgb(175, 82, 222)',
+    red: 'rgb(255, 59, 48)',
+    teal: 'rgb(90, 200, 250)',
+    yellow: 'rgb(255, 204, 0)',
+
+    background: {
+      base: 'white',
+      popup: 'white',
+      light: '#B5B8B9',
+      modal: 'white',
+      modal_mask: 'rgba(255, 255, 255, 0.65)',
+      blur: 'rgba(255, 255, 255, 0.72)',
+    },
+
+    extremeOpposite: 'black',
+    hover: '#f8f8f8',
+
+    text: {
+      title: 'black',
+      body: '#333333',
+      label: '#9597A1',
+      inactive: 'lightgrey',
+    },
+
+    border: '#e6e6e6',
+
+    icon: {
+      primary: '#333333',
+    },
+  },
+}
+
+export default THEME
